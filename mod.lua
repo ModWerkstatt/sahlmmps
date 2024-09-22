43291bc4322e42e0c56ab95288a9db36b9520b3d
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-09-22 14:47:38 +0200

    First

---
+++ mod.lua
@@ -0,0 +1,59 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 2,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "waggon", "schuettgut", "deutschland", "germany", "UIC", "gueterwagen", "db" },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+        params = {
+			{
+				key = "sahlmmps_fake",
+				name = _("Fake_sahlmmps_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_sahlmmps_desc"),
+				defaultIndex = 0,
+			},	
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["sahlmmps_t_xxx_fake.mdl"] = true, 
+			["sahlmmps_t_xxx_fake.mdl"] = true, 
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/sahlmmps-t([^/]*.mdl)')
+							or fileName:match('/salmmnps_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["sahlmmps_fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+		addModifier( "loadModel", metadataHandler )
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,40 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "DB Facs 124 Bogie bulk goods wagon",
+		["mod_desc"] = "The wagons, originally designated Eads-y and procured by the Deutsche Reichsbahn from 1978 onwards, are intended for the transport of bulk goods that are insensitive to moisture. The loading space has the shape of several funnels standing next to each other. The gravity of the load is used for the unloading process. The discharge openings with round slides allow the wagon to be unloaded completely to one side or the other. The swivelling discharge chutes allow bunker unloading.",
+
+        ["Fake_tamns_wagen"] = "Enable Facs 124 Fake Versions?",
+        ["option_fake_tamns_desc"] = "Should the fake versions of the Facs 124 be available in the game?",
+		
+        ["Facs124_menu"] = 		"Facs 124",
+		["Eads-y_dr_name"] = 	"Eads-y DR (1978)",
+		["Eads-y_dr_desc"] = 	"The first wagons were built in France on behalf of the Deutsche Reichsbahn from 1978. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
+       
+		
+		--- Fake ---
+		
+		["Facs124_menu_fake"] = 		"Fake - Facs 124 Fake",
+		["Eads-y_dr_name_fake"] = 		"Fake - Eads-y DR (1978)",
+	},
+	de = {
+		["mod_name"] = "Sahlmmps-t 706.1/ 713 / 721",
+		["mod_desc"] = "Die ursprünglich als Eads-y bezeichneten, von der Deutschen Reichsbahn ab 1978 beschafften Wagen sind zur Beförderung von Nässe unempfindlichem Schüttgut vorgesehen. Der Laderaum hat die Form mehrerer nebeneinander stehender Trichter. Für den Entladevorgang wird die Schwerkraft des Ladegutes genutzt. Die Auslauföffnungen mit Rundschiebern ermöglichen die restlose Entladung des Wagens nach der einen oder der anderen Seite. Die schwenkbaren Auslaufrutschen gestatten eine Bunkerentladung.",
+
+        ["Fake_facs124_wagen"] = "Sahlmmps-t 706.1/ 713 / 721 Fake-Versionen aktivieren?",
+        ["option_fake_facs124_desc"] = "Sollen die Fake-Versionen der Sahlmmps-t 706.1/ 713 / 721 verfügbar sein im Spiel?",
+
+        ["sahlmmps_menu"] = 		"Sahlmmps-t 706.1/ 713 / 721",
+		["sahlmmps_t_706_name"] = 	"Sahlmmps-t 706.1 (1987)",
+		["sahlmmps_desc"] = 		"Die ersten Wagen wurden im Auftrag der Deutschen Reichsbahn ab 1978 in Frankreich gebaut. Die Güterwagen sind geeignet zur Beförderung vom Ladegut wie Sand, Kies, Erz oder Kohle.",
+        
+		
+		--- Fake ---
+		
+		["Facs124_menu_fake"] = 		"Fake - Sahlmmps-t 706.1/ 713 / 721",
+		["Eads-y_dr_name_fake"] = 		"Fake - Eads-y DR (1978)",
+	},
+}
+end
+
+		
