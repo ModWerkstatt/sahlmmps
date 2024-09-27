cd643c833ae797f8dd4b8ad5bd786206159977b1
Florian Thiel <flth@flth.de>
2024-09-27 20:15:01 +0200

    Texte und Übersetzungen überarbeitet

--- strings.lua
+++ strings.lua
@@ -1,28 +1,43 @@
 function data()
 return {
 	en = {
-		["mod_name"] = "DB Facs 124 Bogie bulk goods wagon",
-		["mod_desc"] = "The wagons, originally designated Eads-y and procured by the Deutsche Reichsbahn from 1978 onwards, are intended for the transport of bulk goods that are insensitive to moisture. The loading space has the shape of several funnels standing next to each other. The gravity of the load is used for the unloading process. The discharge openings with round slides allow the wagon to be unloaded completely to one side or the other. The swivelling discharge chutes allow bunker unloading.",
 
-        ["Fake_tamns_wagen"] = "Enable Facs 124 Fake Versions?",
-        ["option_fake_tamns_desc"] = "Should the fake versions of the Facs 124 be available in the game?",
-		
-        ["Facs124_menu"] = 		"Facs 124",
-		["Eads-y_dr_name"] = 	"Eads-y DR (1978)",
-		["Eads-y_dr_desc"] = 	"The first wagons were built in France on behalf of the Deutsche Reichsbahn from 1978. The freight wagons are suitable for transporting loads such as sand, gravel, ore or coal.",
-       
-		
+		["mod_name"] = "Sahlmmps-t 706.1 / 713 / 721",
+		["mod_desc"] = "From 1991, new wagons for the transport of coils were procured by the German Federal Railway. These wagons can load up to 107 tonnes and have a tare weight of between 28 t and 34 t. The wagons are mainly used to transport hot-rolled sheet metal coils.\nThe design of the loading frame allows the loading of sheet metal coils at temperatures of up to 600° C, but the loaded vehicle may only be transported when the temperature on the outer skin of the sheet metal coils has reached 100° C.\nThe loading frame, which weighs around 6 tonnes, is removable. It is secured in four sockets in the wagon floor by means of pins.",
+
+        ["Fake_sahlmmps_wagen"] = "Activate Sahlmmps-t 706.1 / 713 / 721 fake versions?",
+        ["option_fake_sahlmmps_desc"] = "Should the fake versions of the Sahlmmps-t 706.1 / 713 / 721 be available in the game?",
+
+        ["sahlmmps_menu"] = 			"Sahlmmps-t 706.1/ 713 / 721",
+		["sahlmmps_706_name"] = 		"Sahlmmps 706.1 DB (1991)",
+		["sahlmmps_706_dbag_name"] = 	"Sahlmmps 706.1 DB AG (1994)",
+		["sahlmmps_713_name"] = 		"Sahlmmps-t 713 DB AG (2000)",
+		["sahlmmps_721_name"] = 		"Sahlmmps-t 721 DB Schenker (2010)",
+		["sahlmmps_721_hbr_name"] = 	"Sahlmmps-t 721 DB Schenker with handbrake (2010)",
+		["sahlmmps_721_dbag_name"] = 	"Sahlmmps-t 721 DB AG with handbrake (2016)",
+		["sahlmmps_721_dbag2_name"] = 	"Sahlmmps-t 721 DB AG (2016)",
+		["sahlmmps_desc"] = 			"In 1991, the Deutsche Bundesbahn procured new wagons for transporting coils. These wagons can carry up to 107 tonnes and have a tare weight of between 28 t and 34 t",
+
+
 		--- Fake ---
-		
-		["Facs124_menu_fake"] = 		"Fake - Facs 124 Fake",
-		["Eads-y_dr_name_fake"] = 		"Fake - Eads-y DR (1978)",
+
+        ["sahlmmps_menu_fake"] = 			"Fake - Sahlmmps-t 706.1/ 713 / 721",
+		["sahlmmps_706_name_fake"] = 		"Fake - Sahlmmnps 706.1 DB (1991)",
+		["sahlmmps_706_dbag_name_fake"] = 	"Fake - Sahlmmnps 706.1 DB AG (1994)",
+		["sahlmmps_713_name_fake"] = 		"Fake - Sahlmmps-t 713 DB AG (2000)",
+		["sahlmmps_721_name_fake"] = 		"Fake - Sahlmmps-t 721 DB Schenker (2010)",
+		["sahlmmps_721_hbr_name_fake"] =	"Fake - Sahlmmps-t 721 DB Schenker with handbrake (2010)",
+		["sahlmmps_721_dbag_name_fake"] = 	"Fake - Sahlmmps-t 721 DB AG with handbrake (2016)",
+		["sahlmmps_721_dbag2_name_fake"] =	"Fake - Sahlmmps-t 721 DB AG (2016)",
+
 	},
 	de = {
-		["mod_name"] = "Sahlmmps-t 706.1/ 713 / 721",
-		["mod_desc"] = "Die ursprünglich als Eads-y bezeichneten, von der Deutschen Reichsbahn ab 1978 beschafften Wagen sind zur Beförderung von Nässe unempfindlichem Schüttgut vorgesehen. Der Laderaum hat die Form mehrerer nebeneinander stehender Trichter. Für den Entladevorgang wird die Schwerkraft des Ladegutes genutzt. Die Auslauföffnungen mit Rundschiebern ermöglichen die restlose Entladung des Wagens nach der einen oder der anderen Seite. Die schwenkbaren Auslaufrutschen gestatten eine Bunkerentladung.",
 
-        ["Fake_sahlmmps_wagen"] = "Sahlmmps-t 706.1/ 713 / 721 Fake-Versionen aktivieren?",
-        ["option_fake_sahlmmps_desc"] = "Sollen die Fake-Versionen der Sahlmmps-t 706.1/ 713 / 721 verfügbar sein im Spiel?",
+		["mod_name"] = "Sahlmmps-t 706.1 / 713 / 721",
+		["mod_desc"] = "Ab 1991 wurden bei der Deutschen Bundesbahn neue Wagen für den Transport von Coils beschafft. Diese Wagen können bis zu 107 Tonnen zuladen und haben ein Eigengewicht zwischen 28 t und 34 t. Die Wagen dienen hauptsächlich zur Beförderung warm gewalzter Blechrollen (Coils).\nDie Konstruktion des Ladegestells erlaubt die Verladung von bis zu 600° C heißen Blechcoils, befördert werden darf das beladene Fahrzeug jedoch erst, wenn die Temperatur an der Außenhaut der Blechrollen 100° C erreicht hat.\nDas etwa 6 t schwere Ladegestell ist abnehmbar. Es ist in vier Buchsen im Wagenboden durch Zapfen festgelegt.",
+
+        ["Fake_sahlmmps_wagen"] = "Sahlmmps-t 706.1 / 713 / 721 Fake-Versionen aktivieren?",
+        ["option_fake_sahlmmps_desc"] = "Sollen die Fake-Versionen der Sahlmmps-t 706.1 / 713 / 721 verfügbar sein im Spiel?",
 
         ["sahlmmps_menu"] = 			"Sahlmmps-t 706.1/ 713 / 721",
 		["sahlmmps_706_name"] = 		"Sahlmmps 706.1 DB (1991)",
@@ -32,11 +47,11 @@ return {
 		["sahlmmps_721_hbr_name"] = 	"Sahlmmps-t 721 DB Schenker mit Handbremse (2010)",
 		["sahlmmps_721_dbag_name"] = 	"Sahlmmps-t 721 DB AG mit Handbremse (2016)",
 		["sahlmmps_721_dbag2_name"] = 	"Sahlmmps-t 721 DB AG (2016)",
-		["sahlmmps_desc"] = 			"1991 wurde bei der Deutschen Bundesbahn neue Wagen für den Transport von Coils beschafft. Diese Wagen können bis zu 107 Tonnen zuladen und haben ein Eigengewicht zwischen 28t und 34t.",
-        
-		
+		["sahlmmps_desc"] = 			"1991 wurden bei der Deutschen Bundesbahn neue Wagen für den Transport von Coils beschafft. Diese Wagen können bis zu 107 Tonnen zuladen und haben ein Eigengewicht zwischen 28 t und 34 t.",
+
+
 		--- Fake ---
-		
+
         ["sahlmmps_menu_fake"] = 			"Fake - Sahlmmps-t 706.1/ 713 / 721",
 		["sahlmmps_706_name_fake"] = 		"Fake - Sahlmmnps 706.1 DB (1991)",
 		["sahlmmps_706_dbag_name_fake"] = 	"Fake - Sahlmmnps 706.1 DB AG (1994)",
@@ -45,8 +60,7 @@ return {
 		["sahlmmps_721_hbr_name_fake"] =	 "Fake - Sahlmmps-t 721 DB Schenker mit Handbremse (2010)",
 		["sahlmmps_721_dbag_name_fake"] = 	"Fake - Sahlmmps-t 721 DB AG mit Handbremse (2016)",
 		["sahlmmps_721_dbag2_name_fake"] =	"Fake - Sahlmmps-t 721 DB AG (2016)",
+
 	},
 }
 end
-
-		
